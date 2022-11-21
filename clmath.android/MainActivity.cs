64bd7854916c8fd6375ea7fbafec98b57730ec0b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-11-21 09:11:19 +0100

    rudimentary implementation

--- clmath.android/MainActivity.cs
+++ clmath.android/MainActivity.cs
@@ -6,14 +6,17 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
 using Android.Views;
 using Android.Widget;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace clmath.android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        public DrawerLayout drawerLayout { get; private set; }
         public EditText FuncInput { get; private set; }
         public Button BtnEval { get; private set; }
         public TextView ResultOutput { get; private set; }
@@ -23,15 +26,45 @@ namespace clmath.android
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
             
+            // Create UI
+            SetContentView(Resource.Layout.activity_main);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            // Init toolbar
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);	
+
+            // Attach item selected handler to navigation view
+            var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+
+            // Create ActionBarDrawerToggle button and add it to the toolbar
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.open_drawer, Resource.String.close_drawer);
+            drawerLayout.SetDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+
             FuncInput = FindViewById<EditText>(Resource.Id.funcInput)!;
             BtnEval = FindViewById<Button>(Resource.Id.btnEval)!;
             ResultOutput = FindViewById<TextView>(Resource.Id.resultOutput);
             VarsList = FindViewById<LinearLayout>(Resource.Id.varsList);
 
             BtnEval.Click += evalFunc;
+        }
+
+        private void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+        {
+            switch (e.MenuItem.ItemId)
+            {
+                case Resource.Id.nav_calc: break;
+                case Resource.Id.nav_funcs: break;
+                case Resource.Id.nav_constants: break;
+                case Resource.Id.nav_units: break;
+                case Resource.Id.nav_graph: break;
+            }
+
+            // Close drawer
+            drawerLayout.CloseDrawers();
         }
 
         public void evalFunc(object sender, EventArgs args)
