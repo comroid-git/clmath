db56bfd82801884426de04d6894993a71150e9f4
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-31 20:22:37 +0100

    replace xamarin app with maui app

---
+++ clmath.app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace clmath.app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ clmath.app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace clmath.app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
