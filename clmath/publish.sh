49ce6aa360902f7c97edbdcbafa83ec316df422c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-05-09 19:49:18 +0200

    Update publish.sh

--- clmath/publish.sh
+++ clmath/publish.sh
@@ -4,18 +4,21 @@ set -e # exit on error
 
 ./clean.sh
 
+# ensure remote exists
+if [ -z "$(git remote | grep aur)" ]; then
+  git init || true
+  git remote add aur ssh://aur@aur.archlinux.org/clmath-git.git
+fi
+
 # run tests first
 dotnet test -c Test
 
 # update SRCINFO
 makepkg --printsrcinfo > .SRCINFO
-(git add .SRCINFO && git commit -m "SRCINFO" && git push) || (echo "Failed to commit .SRCINFO" >&2 | return)
+(git add .SRCINFO && git commit -m "SRCINFO") || (echo "Failed to commit .SRCINFO" >&2 | exit)
 
 # build the executable
 makepkg -f --noconfirm
 
 # push to aur
-if [ -z "$(git remote | grep aur)" ]; then
-  git remote add aur ssh://aur@aur.archlinux.org/clmath-git.git
-fi
 git push aur
